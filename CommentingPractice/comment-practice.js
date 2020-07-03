97efe48203fe49d7da83e276e40db162479297ff
RamithaKotarkonda <54600694+RamithaKotarkonda@users.noreply.github.com>
2020-07-03 15:49:18 -0500

    added the lowercase function

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -6,6 +6,7 @@ import Template from './model/template.js';
 import * as amonte from './services/capitalizer.js';
 import { myName } from './services/practice.js';
 import * as martin from './services/spaces.js';
+import * as ramitha from './services/lowercase.js';
 
 
 
@@ -18,6 +19,8 @@ import * as martin from './services/spaces.js';
     //used my own function because it doesnt seem like anybody else added something
     var space = "space is large";
      console.log(amonte.randomlyCapitalize(space));
+    var word = "WORD";
+    console.log(amonte.lowercase(word));
  })()
 
 
