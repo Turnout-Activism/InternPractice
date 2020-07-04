0eb9356bd5813c935090ea2ebc2d31af0f3bec69
T Malik <tmalik8@masonlive.gmu.edu>
2020-07-04 12:29:15 -0400

    Javascript Commit

---
+++ CommentingPractice/JavaScript Commit.js
@@ -0,0 +1,18 @@
+/**This is my first comment
+ * Represents a car
+ * @constructor
+ * @param {int} num_doors
+ * @param {string} brand
+ * @param {string} color
+ */
+
+ function Car (num_doors,brand,color)
+ {
+ 	return "The car has " + string(num_doors) + " and is a " + color + brand + ".";
+ }
+ for (var x = 0; x<10;x++)
+ {
+  console.log(x);
+ }
+ console.log("\n");
+ console.log(Car(4,"Toyota","red"));
