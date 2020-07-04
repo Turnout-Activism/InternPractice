a2c59b5bfbebfe4146a97852a48a5f20201acd14
T Malik <tmalik8@masonlive.gmu.edu>
2020-07-04 18:36:48 -0400

    Javascript commit

--- CommentingPractice/JavaScript Commit.js
+++ CommentingPractice/JavaScript Commit.js
@@ -1,18 +1,24 @@
 /**This is my first comment
- * Represents a car
- * @constructor
+ * Represents a van
+ * @class
  * @param {int} num_doors
  * @param {string} brand
  * @param {string} color
  */
 
- function Car (num_doors,brand,color)
+ class Van (num_doors,brand,color)
  {
- 	return "The car has " + string(num_doors) + " and is a " + color + brand + ".";
+    this.doors = num_doors;
+    this.brand = brand;
+    this.color = color;
  }
- for (var x = 0; x<10;x++)
+ 
+ print() 
  {
-  console.log(x);
+    return "My van is a " + this.color + this.brand + "and has " + this.doors + "."
  }
- console.log("\n");
- console.log(Car(4,"Toyota","red"));
+
+ van1 = new Van(4,"Toyota","red");  
+
+ document.getElementById("demo").innerHTML = van1.print();
+
