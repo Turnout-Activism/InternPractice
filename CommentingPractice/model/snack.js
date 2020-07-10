cd13304c7b9776b07e7f33ad037e466cbbeb41f8
Cesenor <Senorce@outlook.com>
2020-07-10 17:48:27 -0500

    Update snack.js
    Fixing commenting

--- CommentingPractice/model/snack.js
+++ CommentingPractice/model/snack.js
@@ -12,37 +12,37 @@ export default class Snack{
     constructor(typeOfSnack, isHealthy){
     var type;
     var heathly;
-}//closes constructor
+    }//closes constructor
 
-/**
-* getType() setter method sets type field
-*/
-getType(){
-    return this.type;
-}
+    /**
+    * getType() setter method sets type field
+    */
+    getType(){
+        return this.type;
+    }
 
-/**
-*  Sets the snack field.
-* @param {"String"} typePara - The type of snack
-*/
-setType(typePara){
-    this.type = typePara;
-}
+    /**
+    *  Sets the snack field.
+    * @param {"String"} typePara - The type of snack
+    */
+    setType(typePara){
+        this.type = typePara;
+    }
 
-/**
-* Gets the healthy field.
-*/
-getHealthy(){
-    return this.heathly;
-}
+    /**
+    * Gets the healthy field.
+    */
+    getHealthy(){
+        return this.heathly;
+    }
 
-/**
-* Sets the health field.
-* @param {"boolean"} healthyPara - True or false if snack is healthy or not.
-*/
-setHealthy(healthyPara){
-this.heathly = healthyPara;
-}
+    /**
+    * Sets the health field.
+    * @param {"boolean"} healthyPara - True or false if snack is healthy or not.
+    */
+    setHealthy(healthyPara){
+    this.heathly = healthyPara;
+    }
 } //Closes class
 
 
@@ -51,46 +51,42 @@ this.heathly = healthyPara;
 */
 export class Chocolate extends Snack{
 
-/**
- * Constructor
- */
-constructor(){
-    super("Candy", false) //calling constructor or paraent class
-    var cocoPowderPercentage;
-    var chocolateType;
-}
+    /**
+    * Constructor
+    */
+    constructor(){
+        super("Candy", false) //calling constructor or paraent class
+        var cocoPowderPercentage;
+        var chocolateType;
+    }
 
-/**
- * Gets the cocoPowderPercentage field.
- */
-getCocoPowderPercentage(){
-    return cocoPowderPercentage;
+    /**
+    * Gets the cocoPowderPercentage field.
+    */
+    getCocoPowderPercentage(){
+        return cocoPowderPercentage;
+    }
 
-}
+    /**
+    * Sets the cocoPowderPercentage field.
+    * @param {String} percentagePara 
+    */
+    setCocoPowderPercentage(percentagePara){
+        this.cocoPowderPercentage = percentagePara;
+    }
 
+    /**
+    * Gets the chocolateType field
+    */
+    getChocolateType(){
+        return this.chocolateType;
+    }
 
-/**
- * Sets the cocoPowderPercentage field.
- * @param {String} percentagePara 
- */
-setCocoPowderPercentage(percentagePara){
-    this.cocoPowderPercentage = percentagePara;
-}
-
-/**
- * Gets the chocolateType field
- */
-getChocolateType(){
-    return this.chocolateType;
-}
-
-/**
- * Sets the chocolate type (milk, dark, white)
- * @param {String} typePara 
- */
-setChocolateType(typePara){
-    this.chocolateType = typePara;
-}
-
-
+    /**
+    * Sets the chocolate type (milk, dark, white)
+    * @param {String} typePara 
+    */
+    setChocolateType(typePara){
+        this.chocolateType = typePara; 
+    }
 }
