6312e9962de42f783dd14f97ea1fd401f676ea5c
T Malik <tmalik8@masonlive.gmu.edu>
2020-07-12 13:53:58 -0400

    Updated teammate.js

--- CommentingPractice/model/teammate.js
+++ CommentingPractice/model/teammate.js
@@ -1,13 +1,44 @@
 /**
  * @file TeamMate class
  * @author Luis Moran (Web Development)
+ * @author Edited by Tanya Malik
  */
 
 /**
  * @module
  */
+class Team{
+    /**
+     * @constructor main Team class
+     * @param {string} tname 
+     * @param {number} numMembers
+     */
+     constructor(tname,numMembers)
+     {
+       this.tname=tname;
+       this.numMembers = numMembers;
+     }
+       //function will return the team name if set.
+    getTeamName()
+    {
+      return this.tName; 
+    }
 
- export default class TeamMate {
+    setTeamName(tName)
+    {
+      this.tName = tName; 
+    }
+    getNumMembers()
+    {
+      return this.numMembers; 
+    }
+    setNumMembers(numMembers)
+    {
+      this.numMembers = numMembers;
+    }
+ }
+
+class TeamMate extends Team {
     
     /** 
     * @description creates a Teammate object that will have the first name and last name, with a choice to set a team name. 
@@ -15,36 +46,26 @@
     * Constructor
     * @param {string} fName - is first Name 
     * @param {string} lName - is last name
-    * 
+    * @param {Array} strengths - strengths of team member
     */
-    constructor(fName, lName){
+    constructor(tname,numMembers,fName, lName,strengths){
+        super(tname,numMembers);
         this.fName = fName; 
         this.lName = lName;
-
-        let tName; 
+        this.strengths  = strengths;
     }
 
-    //function will return the team name if set.
-    getTeamName(){
-        return this.tName; 
+   
+    getStrengths()
+    {
+        return this.strengths;
     }
-
-    //function setTeamName will alow to set a team name. 
-    /**
-     * @param {string} tname - Team name 
-     */
-    setTeamName(tName){
-        this.tName = tName; 
+    setStrengths(strengths)
+    {
+        this.strengths = strengths;
     }
-
-    //function hello will return statement using instances for first name, last name and team name
-    hello(){
-        console.log(`Hello my name is ${this.fName} ${this.lName} and I am on team: ${this.tName === undefined ? 'none': this.tName }`)
-    }
-
 }
 
-//Test Enviornment for class. 
-// const obito = new TeamMate('Obito', 'Cool'); 
-// // obito.setTeamName('Web Development'); 
-// obito.hello(); 
+const teammate = new TeamMate("Hoosiers",30,"Tanya","Malik",['helping the team in any way I can','Python','asking for help where needed']);
+console.log("My name is " + teammate.fName + " " + teammate.lName + ". My team is called " + teammate.tname + " and there are " + teammate.numMembers + " members in my team.");
+console.log("I am good at " + teammate.strengths[0] + ", coding in " + teammate.strengths[1] + ", and " + teammate.strengths[2] + ".");
