6facde9126bdf8e6798835fe5b406be5b35572f6
idocats <57145809+idocats@users.noreply.github.com>
2020-07-17 13:01:14 -0400

    Update allServices.js

--- CommentingPractice/test/allServices.js
+++ CommentingPractice/test/allServices.js
@@ -1,8 +1,9 @@
 /**
  * @file All services module export
  */
-const CapitalizerService = require('../services/capitalizer') ;
-const CaeserCipherService = require('../services/caesarCipher')
+
+'use strict';
+
 
 import Template from '../model/template.js';
 import * as RandomCapitalizer from '../services/capitalizer.js';
@@ -15,5 +16,7 @@ import * as Product from '../services/product.js';
 import * as Arithmetic from '../services/arithmetic.js';
 import * as CaeserCipher from '../services/caesarCipher.js';
 import * as Splitter from '../services/splitor.js';
+import * as SumOfNumber from '../services/sumOfNumber.js';
+import * as Sum from '../services/sum.js';
 
- module.exports = {Template, RandomCapitalizer, myName, Spaces, Backwards, toPigLatin, Lowercase, Product, Arithmetic, CaeserCipher, Splitter}
+export default {Template, RandomCapitalizer, myName, Spaces, Backwards, toPigLatin, Lowercase, Product, Arithmetic, CaeserCipher, Splitter, SumOfNumber,Sum}
