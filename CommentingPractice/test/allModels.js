5bcb6d1e19dbd57fb5a71135dfd290eda7c13130
idocats <57145809+idocats@users.noreply.github.com>
2020-07-17 13:01:12 -0400

    Create allModels.js

---
+++ CommentingPractice/test/allModels.js
@@ -0,0 +1,9 @@
+import * as Animals from '../model/animal.js'
+import * as Languages from '../model/language.js'
+import * as Plants from '../model/plant.js'
+import * as Libraries from '../model/library.js'
+import * as RomanEmpire from '../model/rome.js'
+import * as Students from '../model/Student.js'
+import * as Teammates from '../model/teammate.js'
+
+export {Animals, Languages, Plants, Libraries, RomanEmpire, Students, Teammates}
