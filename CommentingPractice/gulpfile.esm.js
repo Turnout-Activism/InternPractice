b5f61c2d23e0ccef14ffb57f0fd0648e2bad5823
idocats <57145809+idocats@users.noreply.github.com>
2020-07-17 13:00:40 -0400

    Delete gulpfile.esm.js

--- CommentingPractice/gulpfile.esm.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const jsdoc = require('gulp-jsdoc3');
-
-gulp.task('doc', function (cb) {
-    gulp.src(['README.md', './model/*.js', './services/*.js'], {read: false})
-        .pipe(jsdoc(cb));
-});
-
